80be12e43a1a377f7cfc167e5c4383efd97f9d40
Leonardo Furmann <leohitnerf@gmail.com>
2024-06-29 22:04:25 -0300

    correção lógica dos filmes que já foram

--- script.js
+++ script.js
@@ -4,20 +4,11 @@ import * as api from "./api.js"
 let movies = [];
 let temp = [];
 let moviesRound = [];
-const tamanho = 0;
+let tamanho = 0;
 
 const options = { method: 'GET', headers: { accept: 'application/json' } };
 const base_url_img = "https://image.tmdb.org/t/p/original"
-
-
 const contador = document.getElementById("contador");
-
-const imgFilme1 = document.getElementById("img_movie1");
-const imgFilme2 = document.getElementById("img_movie2");
-const nameMovie1 = document.getElementById("name_movie1");
-const nameMovie2 = document.getElementById("name_movie2");
-
-
 const div_movies = document.querySelectorAll('.movie');
 
 div_movies.forEach((div_movie) => {
@@ -30,6 +21,7 @@ async function fetchMovies() {
   const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${api.api_key}&language=pt-BR&page=1`, options);
   const data = await response.json();
   movies = data.results.slice(0, 16);
+  tamanho = movies.length;
   console.log('fetch')
 }
 
@@ -43,58 +35,48 @@ async function startMovies() {
   // } 
 }
 
-async function startRound() {
-
-  shuffle(movies);
-  contador.innerText = `${movies.length} filmes`;
-  
-
-  div_movies.forEach((movie, index) => {
-    movie.children[0].src = base_url_img + movies[index].poster_path  
-    movie.children[1].innerText = movies[index].title;
-    movie.dataset.id = movies[index].id;
-    moviesRound.unshift(movies[index]);
-    removeMovie(movies[index]) //remove filme da lista, pq ele já foi
-  })
-
-  console.log(movies)
-
-}
-
-
-async function selectMovie(div_movie) {
-
-  if (temp.length == (movies.length / 2)) {
-
+async function startRound() { 
+  if (temp.length === (tamanho / 2)) {
+    console.log("temp if ", temp.length)
+    tamanho = (tamanho / 2);
+    contador.innerText = `${tamanho} filmes`;
     movies = temp;
     temp = [];
-    console.log("movies ", movies)
-    console.log("temp ", temp)
-    await startRound();
-    return
   }
 
-  const selectedMovie = getMovieById(div_movie.dataset.id);
-  temp.unshift(selectedMovie);
-  
-  if (movies.length == 2 && temp.length == 0) {
-    console.log(`o filme ${selectedMovie.title} ganhou!!!!` );
-    return
-  }
+  shuffle(movies);
 
-
-  moviesRound = [];
-  await startRound()
   console.log("movies ", movies)
-  console.log("temp ", temp)
+
+  div_movies.forEach((movie) => {
+    console.log(movies[0]);
+    movie.children[0].src = base_url_img + movies[0].poster_path
+    movie.children[1].innerText = movies[0].title;
+    movie.dataset.id = movies[0].id;
+    moviesRound.unshift(movies[0]);
+    movies.shift() //remove filme da lista, pq ele já foi
+  })
 
 }
 
-function getRandom(array){
-   const num = Math.floor(Math.random() * (array.length - 1));
+async function selectMovie(div_movie) {
+  const selectedMovie = getMovieById(div_movie.dataset.id);
+  temp.unshift(selectedMovie);
+
+  if (movies.length == 0 && temp.length == 0) {   //verificar depois
+    console.log(`o filme ${selectedMovie.title} ganhou!!!!`);
+    return
+  }
+  moviesRound = [];
+  await startRound()
+  console.log("temp ", temp.length)
+}
+
+function getRandom(array) {
+  const num = Math.floor(Math.random() * (array.length - 1));
   const movie = array[num];
   movie.jaFoi = true;
-   return array[num];
+  return array[num];
 }
 
 function shuffle(array) {
@@ -111,7 +93,7 @@ function shuffle(array) {
 function removeMovie(movie) {
   const index = movies.indexOf(movie);
   movies.splice(index, 1);
-
+ console.log("filme removido", movies[index].title)
 }
 
 function getMovieById(id) {
